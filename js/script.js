b9db690bfdd2a3451c9425c2e5e578abcc2590ff
jinlong25 <jinlong25@gmail.com>
2016-02-03 20:51:28 -0800

    init commit - set up a simple map using mapbox.js

---
+++ js/script.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiamlubG9uZyIsImEiOiJhMWUzNzk1MTEyNTUyNzkyNzBjZWUzYWMwODM2ZjgyZiJ9.youixT7oBlwLEwXC9q3P3w';
+var map = L.mapbox.map('map', 'mapbox.streets')
+	.setView([37.7833, -122.4167], 12);
