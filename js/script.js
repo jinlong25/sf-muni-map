9a63e703c4bc3e548292ab7fb607a236c7060bc2
jinlong25 <jinlong25@gmail.com>
2016-02-10 01:41:46 -0800

    add info window and speed window

--- js/script.js
+++ js/script.js
@@ -9,16 +9,36 @@ var locs,
 	routeList,
 	locGeoJSON,
 	routeGeoJSON,
+	speedLine,
+	selectedBus,
 	currentZoomLevel = 13,
 	updateInterval = 5000,
 	duration = 5000;
 
-//define point size at all zoom scales
-var PointScaleReference = {10: '0.05', 11: '0.2', 12: '0.5', 13: '3', 14: '3.5', 15: '4', 16: '4.2', 17: '6', 18: '8', 19: '9', 20: '9',21: '9', 22: '9'}
+
+//define vars of speed chart
+var sc = {
+	w: 200,
+	h: 50,
+	top: 30,
+	right: 10,
+	bottom: 20,
+	left: 30,
+	duration: 1000,
+	// xAxis,
+	// yAxis,
+	// numFormatter: d3.format(','),
+	// dateParser: d3.time.format('%Y-%m-%d').parse,
+	// tooltipDateParser: d3.time.format('%b.%e')
+	line: d3.svg.line()
+							.x(function(d) { return sc.x(d.time); })
+							.y(function(d) { return sc.y(d.speed); })
+							.interpolate('monotone')
+};
 
 //set sf bbox
 var sf = { 'type': 'FeatureCollection', 'features': [ { 'type': 'Feature', 'geometry': { 'type': 'Point', 'coordinates': [-122.515149, 37.812780] } },
-{ 'type': 'Feature', 'geometry': { 'type': 'Point', 'coordinates': [-122.352600, 37.712321] } } ] }
+{ 'type': 'Feature', 'geometry': { 'type': 'Point', 'coordinates': [-122.352600, 37.652321] } } ] }
 
 //setup map
 var map = L.mapbox.map('map', 'mapbox.dark', {maxZoom: 16, minZoom: 12})
@@ -40,26 +60,29 @@ initializeRouteList();
 initializeVehicleLocation(routeTag);
 
 //update vehicle locations every xx seconds
+setTimeout(function() {
+	updateVehicleLocation(routeTag)
+}, 500);
 setInterval(function() {
 	updateVehicleLocation(routeTag)
 }, updateInterval);
-// setTimeout(updateLocation, 3000);
+
 
 //create svg in info window
-var canvas = d3.select('#info_window').append('svg')
+var canvas = d3.select('#speed_chart_window').append('svg')
 	.attr('class', 'canvas')
-	.attr('width', 800)
-	.attr('height', 200)
+	.attr('width', sc.w + sc.left + sc.right)
+	.attr('height', sc.h + sc.top + sc.bottom)
 	.append('g')
-	.attr('transform', 'translate(10, 10)');
+	.attr('transform', 'translate(' + sc.left + ',' + sc.top + ')');
 
-//draw
-// Bus Route:
-// From ## to ##
-// # of fleet:
-// Active buses:
-//
-// canvas.
+//create a background for reference TODO remove
+// canvas.append('rect')
+// 	.attr('width', sc.w)
+// 	.attr('height', sc.h)
+// 	.style('fill', '#fff');
+
+var speedChart = canvas.append('g').attr('class', 'speed-chart');
 
 // ------------------------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------  functions -------------------------------------------------------
@@ -89,16 +112,19 @@ function initializeRouteList() {
 				d3.selectAll('.bus').remove();
 				d3.selectAll('.route').remove();
 
+				//remove speed line
+				d3.selectAll('.speed-line').remove();
+
+				//remove all axes
+				d3.selectAll('.axis').remove();
+
 				//re-initialize buses and route
-				var options = document.getElementById('route_selection');
+				options = document.getElementById('route_selection');
 				routeTag = options.options[options.selectedIndex].value;
 
 				//call to initialize vehicle locations
 				initializeVehicleLocation(routeTag);
 			});
-
-			//set default route
-			// d3.select('option').attr('selected', 'N');
 		}
 	});
 }
@@ -114,7 +140,6 @@ function initializeVehicleLocation(routeTag) {
 			success: function(xml, textStatus) { //TODO add error handling case
 				//refactor vehicle info into GeoJSON
 				locGeoJSON = parsingVehicleLoc(xml);
-				console.log(locGeoJSON.features.length + ' buses');
 			}
 		}),
 
@@ -126,6 +151,10 @@ function initializeVehicleLocation(routeTag) {
 			success: function(xml, textStatus) { //TODO add error handling case
 				//parse route response
 				routeGeoJSON = parsingRoute(xml);
+				var routeInfo = parsingRouteInfo(xml);
+				d3.select('#route_name').text(routeInfo[2]);
+				d3.select('#from_stop').text(routeInfo[0]);
+				d3.select('#to_stop').text(routeInfo[1]);
 
 				//set map view based on route
 				bbox = turf.extent(routeGeoJSON);
@@ -150,6 +179,9 @@ function initializeVehicleLocation(routeTag) {
 				return d.properties.busId;
 			});
 
+		//update fleet size info
+		d3.select('#fleet_size').text(locGeoJSON.features.length);
+
 		//trigger reset on map viewreset
 		map.on('viewreset', reset);
 		reset();
@@ -171,17 +203,63 @@ function initializeVehicleLocation(routeTag) {
 			g.attr('transform', 'translate(' + -topLeft[0] + ',' + -topLeft[1] + ')');
 
 			//update path
-			// path.pointRadius(PointScaleReference[map.getZoom()]);
 			path.pointRadius(3);
 
 			locs.data(locGeoJSON.features, function(d) {
 				return d.properties.busId;
 			}).transition()
-				.duration(259)
+				.duration(250)
 				.attr('d', path);
 
 			route.attr('d', path);
 		}
+
+		//get selected bus data
+		selectedBus = locGeoJSON.features[0]; //TODO get selected bus by click
+		var selectedBusData = [];
+
+		//compose data for speed chart
+		for (var i = 0; i < selectedBus.properties.timestamp.length; i++) {
+			d = {'time': selectedBus.properties.timestamp[i],
+				'speed': selectedBus.properties.speedKmHr[i]
+			}
+			selectedBusData.push(d);
+		}
+
+		//define x/y scales
+		sc.x = d3.time.scale()
+			.range([0, sc.w])
+			.domain([selectedBus.properties.timestamp[0],
+				selectedBus.properties.timestamp.slice().pop()]);
+
+		var maxSpeed = d3.max(selectedBus.properties.speedKmHr, function(d) { return d; }) * 1.5;
+		sc.y = d3.scale.linear()
+			.range([0, sc.h])
+			// .domain([maxSpeed === 0 ? 100 : maxSpeed, 0]);
+			.domain([100, 0]);
+
+		//Draw speed line
+		speedLine = speedChart.append('path')
+			.attr('class', 'speed-line')
+			.attr('d', sc.line(selectedBusData));
+
+		//define axes
+		sc.xAxis = d3.svg.axis().scale(sc.x).orient('bottom').ticks(2)
+			.tickFormat(d3.time.format('%H:%M:%S'));
+
+		sc.yAxis = d3.svg.axis().scale(sc.y).orient('left')
+			.ticks(2)
+			.tickFormat(function(d) { return d === 0 ? '' : d; });
+
+		//draw axes
+		canvas.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(0, ' + sc.h + ')')
+			.call(sc.xAxis);
+
+		canvas.append('g')
+			.attr('class', 'y axis')
+			.call(sc.yAxis);
 	});
 }
 
@@ -227,15 +305,31 @@ function updateVehicleLocation(routeTag) {
 			locs.data(locGeoJSON.features, function(d) {
 				return d.properties.busId;
 			})
-				.attr('lon', function(d) {
-					return d.geometry.coordinates[0];
-				})
-				.attr('lat', function(d) {
-					return d.geometry.coordinates[1];
-				})//TODO add more attrs
 				.transition()
 				.duration(duration)
 				.attr('d', path);
+
+				//click to select selected bus
+				d3.selectAll('.bus').on('click', function() {
+					var selectedBusId = d3.select(this).attr('id');
+					//dehightlight non-selected bus
+					d3.selectAll('.bus').style('fill', 'red');
+					d3.selectAll('.bus').style('stroke', 'yellow');
+
+					//highlight selected bus
+					d3.select(this).style('fill', 'yellow');
+					d3.select(this).style('stroke', 'red');
+
+					//update selected bus data
+					for (var i = 0; i < locGeoJSON.features.length; i++) {
+						if (locGeoJSON.features[i].properties.busId === selectedBusId) {
+							selectedBus = locGeoJSON.features[i];
+							updateSpeedLine(selectedBus);
+						}
+					}
+				});
+
+				updateSpeedLine(selectedBus);
 		}
 	});
 }
@@ -253,7 +347,7 @@ function parsingVehicleLoc(xml) {
 		'features': []
 	};
 
-	var currentTimestamp = new Date().getTime() / 1000;
+	var currentTimestamp = new Date().getTime();
 
 	var vehicles = $(xml).find('vehicle');
 
@@ -310,6 +404,22 @@ function parsingRoute(xml) {
 	return output;
 }
 
+//parsing XML response of route to return begin and end stops
+function parsingRouteInfo(xml) {
+	var output = [];
+
+	var stops = $(xml).find('route>stop');
+	stops.each(function(i, e) {
+		var stop = $(e).attr('title');
+		output.push(stop);
+	});
+
+	var route = $(xml).find('route');
+	var routeName = $(route).attr('title');
+
+	return [output[0], output.pop(), routeName];
+}
+
 //parsing XML response of route list info into a single GeoJSON object
 function parsingRouteList(xml) {
 	var output = [];
@@ -320,4 +430,44 @@ function parsingRouteList(xml) {
 	});
 
 	return output;
+}
+
+//update speed line
+function updateSpeedLine(selectedBus) {
+	//update the selected bus data
+	selectedBusData = [];
+
+	//compose data for speed chart
+	for (var i = 0; i < selectedBus.properties.timestamp.length; i++) {
+		d = {'time': selectedBus.properties.timestamp[i],
+			'speed': selectedBus.properties.speedKmHr[i]
+		}
+		selectedBusData.push(d);
+	}
+
+	//update x/y scales
+	sc.x.domain([selectedBus.properties.timestamp[0],
+			selectedBus.properties.timestamp.slice().pop()]);
+
+	// var maxSpeed = d3.max(selectedBus.properties.speedKmHr, function(d) { return d; }) * 1.5;
+	// sc.y.domain([maxSpeed === 0 ? 100 : maxSpeed, 0]);
+
+	//update speed line
+	speedLine.transition()
+		.duration(500)
+		.attr('d', sc.line(selectedBusData));
+
+	//update axes
+	sc.xAxis.scale(sc.x);
+	sc.yAxis.scale(sc.y);
+
+	//update drawing axes
+	d3.select('.x.axis')
+		.transition()
+		.duration(200)
+		.call(sc.xAxis);
+	d3.select('.y.axis')
+		.transition()
+		.duration(200)
+		.call(sc.yAxis);
 }
